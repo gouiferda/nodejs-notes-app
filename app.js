896070b46402e8ca2e4bd44fa16c05787612eb96
Soufiane <soufiane.gouiferda@gmail.com>
2020-04-21 10:04:53 +0000

    updated

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+const validator = require('validator')
+
 const getNotes = require('./notes.js')
 
 const msg = getNotes()
 
 console.log(msg)
+
+console.log(validator.isEmail('ahahah@kk.de'))
+
+console.log(validator.isURL('kkede'))
